9576bcab9ed0826681e0a17a715a46b656346ccb
hikojlu <extrafreevirusmailbox@gmail.com>
2025-03-05 19:10:43 +0200

    new examples

--- main.py
+++ main.py
@@ -6,8 +6,18 @@ root = tk.Tk()
 root.geometry("500x500")
 root.title("hey")
 
-test = tk.Label(text="test")
+testtw = ttk.Treeview(root, columns=("t1", "t2"))
+testtw.heading("#0", text="test 0",)
+testtw.heading("t1", text="test 1")
+testtw.heading("t2", text="test 2")
 
-test.place(relx=0.1, rely=0.1, relwidth=0.5, relheight=0.2)
+testtw.insert(
+    "",
+    tk.END,
+    text="keyvalue",
+    values=("test1value", "test2value")
+)
+
+testtw.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.6)
 
 root.mainloop()

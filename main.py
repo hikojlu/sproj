250c23fa5c1c8bd80ed7302b4f816bfcd87ec988
hikojlu <extrafreevirusmailbox@gmail.com>
2025-03-05 18:56:49 +0200

    minor changes

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import tkinter as tk
+from tkinter import ttk
 
 #relx rely relwidth relheight
 root = tk.Tk()
+root.geometry("500x500")
+root.title("hey")
 
-root.geometry('500x500')
-root.title('hey')
+test = tk.Label(text="test")
 
-test = tk.Label(text = 'test')
-test.place(relx=0.1,rely=0.1,relwidth=0.5,relheight=0.2)
+test.place(relx=0.1, rely=0.1, relwidth=0.5, relheight=0.2)
 
 root.mainloop()
